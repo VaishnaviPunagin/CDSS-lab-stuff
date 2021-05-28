363c05a7ce870be991da249d802ee47388869413
PercyJackson <dumdumwantsagumgum@gmail.com>
2021-05-28 23:14:52 +0530

    Enter SAMPLE for 8th one LOL

--- 8/8.c
+++ 8/8.c
@@ -1,43 +1,45 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void main()
+int main()
 {
-FILE *fp;
-int i,addrl,l,j,staddrl;
-char name[10],line[50],namel[10],addr[10],rec[10],ch,staddr[10];
-printf("enter program name:");
-scanf("%s",name);
-fp=fopen("abssrc.txt","r");
-fscanf(fp,"%s",line);
-for(i=2,j=0;i<8,j<6;i++,j++)
-namel[j]=line[i];
-namel[i]='\0';
-printf("name from obj.%s\n",namel);
-if(strcmp(name,namel)==0)
-{
-	do{
-		fscanf(fp,"%s",line);
-		if(line[0]=='T'){
-			for(i=2,j=0;i<8,j<6;i++,j++)
-				staddr[j]=line[i];
-				staddr[j]='\0';
-				staddrl=atoi(staddr);
-				i=12;
-		while(line[i]!='$')
-		{
-		if(line[i]!='^')
-		{
-		printf("00%d\t%c%c\n",staddrl,line[i],line[i+1]);
-		staddrl++;
-		i=i+2;
-		}
-		else
-			 i++;
-		}}
-	else if(line[0]='E')
-	fclose(fp);
-	}
-	while(!feof(fp));
-}
+    FILE *fp;
+    int i,j,staddr1;
+    char name[10],line[50],name1[10],staddr[10];
+    printf("enter program name:" );
+    scanf("%s",name);
+    fp=fopen("abssrc.txt","r");
+    fscanf(fp,"%s",line);
+    for(i=2,j=0;i<8,j<6;i++,j++)
+        name1[j]=line[i];
+    name1[j]='\0';
+    printf("name from obj. %s\n",name1);
+    if(strcmp(name,name1)==0)
+    {
+        do
+        {
+            fscanf(fp,"%s",line);
+            if(line[0]=='T')
+            {
+                for(i=2,j=0;i<8,j<6;i++,j++)
+                    staddr[j]=line[i];
+                staddr[j]='\0';
+                staddr1=atoi(staddr);
+                i=12;
+                while(line[i]!='$')
+                {
+                    if(line[i]!='^')
+                    {
+                        printf("00%d \t %c%c\n", staddr1,line[i],line[i+1]);
+                        staddr1++;
+                        i=i+2;
+                    }
+                    else i++;
+                }
+            }
+            else if(line[0]='E')
+                fclose(fp);
+        }while(!feof(fp));
+    }
+    return 0;
 }
